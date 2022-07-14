083c2c15cb1b34b6dd13c21e30a4a3fe6ae23669
Henry Taby <henry.taby@gmail.com>
2022-07-14 12:44:04 -0400

    []creación del Proyecto con Maven y Tests Unitarios

---
+++ src/main/java/com/taby/java_test/util/StringUtil.java
@@ -0,0 +1,11 @@
+package com.taby.java_test.util;
+
+public class StringUtil {
+    public static String repeat(String str, int times){
+        String result = "";
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+        return result;
+    }
+}
