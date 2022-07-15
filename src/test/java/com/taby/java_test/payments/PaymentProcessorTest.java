cf3f09fb87702d375c735c16fb2846cf43433753
Henry Taby <henry.taby@gmail.com>
2022-07-15 10:44:24 -0400

    Análisis de los tests y mejoras

--- src/test/java/com/taby/java_test/payments/PaymentProcessorTest.java
+++ src/test/java/com/taby/java_test/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.taby.java_test.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -7,22 +8,29 @@ import org.mockito.Mockito;
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
     @Test
     public void payment_is_correct() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+        //Organizar el esenario
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
-        assertTrue(paymentProcessor.makePayment(1000));
+        // Llamada al metodo a probar
+        boolean result = paymentProcessor.makePayment(1000);
+        // comprobar si el resultado es el esperado
+        assertTrue(result);
     }
 
     @Test
     public void payment_is_wrong() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
