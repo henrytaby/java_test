d623f1bf864117b536e623dc0d27fc0dfff722ab
Henry Taby <henry.taby@gmail.com>
2022-07-15 10:02:52 -0400

    Test con Mockito para simular un dado

---
+++ src/test/java/com/taby/java_test/player/PlayerTest.java
@@ -0,0 +1,23 @@
+package com.taby.java_test.player;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void loose_when_dice_number_is_too_low() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+        Player player = new Player(dice,3);
+        assertFalse(player.play());
+    }
+    @Test
+    public void loose_when_dice_number_is_big() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+        Player player = new Player(dice,3);
+        assertTrue(player.play());
+    }
+}
